4062d9425bb0b39ba73770aea64c00dcfd8773f4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Create ASPxGridView with a custom pager and the "Selecting a page size" feature at runtime
+
+
+<p>A custom pager implementation can be performed by using separate controls. However, under certain circumstances (due to the control hierarchy recreation), it is difficult to implement this feature using a server-side code only.</p><p>This example demonstrates how to achieve the desired result using ASPxComboBoxes within the PageBar template.</p><p><strong>See Also:</strong><strong><br />
+</strong><a href="https://www.devexpress.com/Support/Center/p/E1738">How to create a custom pager for the ASPxGridView with the "Selecting a page size" feature</a><u><br />
+</u><a href="https://www.devexpress.com/Support/Center/p/E358">Create the custom pager in the status bar template</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,57 @@
+﻿Imports Microsoft.VisualBasic
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxGridView
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Init(ByVal sender As Object, ByVal e As EventArgs)
+		CreateGridView()
+	End Sub
+
+	Private Sub CreateGridView()
+		Dim grid As ASPxGridView = New ASPxGridView With {.ID = "grid", .DataSource = sds, .KeyFieldName = "ProductID", .ClientInstanceName = "grid", .EnableRowsCache = False}
+		grid.SettingsPager.AlwaysShowPager = True
+		AddHandler grid.Load, AddressOf grid_Load
+		AddHandler grid.CustomCallback, AddressOf grid_CustomCallback
+		AddHandler grid.CustomJSProperties, AddressOf grid_CustomJSProperties
+		grid.Templates.PagerBar = LoadTemplate("CustomPager.ascx")
+		Page.Form.Controls.Add(grid)
+		grid.DataBind()
+	End Sub
+	Protected Sub grid_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+		Dim position As String = String.Empty
+		If cbPagerPosition.Value IsNot Nothing Then
+			position = cbPagerPosition.Value.ToString()
+		End If
+		Select Case position
+			Case "PosTop"
+				grid.SettingsPager.Position = PagerPosition.Top
+			Case "PosBoth"
+				grid.SettingsPager.Position = PagerPosition.TopAndBottom
+			Case Else
+				grid.SettingsPager.Position = PagerPosition.Bottom
+		End Select
+	End Sub
+
+	Protected Sub grid_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+		Dim newPageSize As Integer
+		If e IsNot Nothing Then
+			If (Not Integer.TryParse(e.Parameters, newPageSize)) Then
+				Return
+			End If
+			grid.SettingsPager.PageSize = newPageSize
+		End If
+	End Sub
+	Protected Sub grid_CustomJSProperties(ByVal sender As Object, ByVal e As ASPxGridViewClientJSPropertiesEventArgs)
+		Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+		e.Properties("cpPageSize") = grid.SettingsPager.PageSize
+	End Sub
+End Class
